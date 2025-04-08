6e5db9e4722cebf055c61390d547d8fd3df8d0a0
leejung7308 <leekkzzzz@naver.com>
2025-04-09 01:14:22 +0900

    프로젝트 초기 세팅

---
+++ Source/mmorpg/mmorpg.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "mmorpg.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mmorpg, "mmorpg" );
+
+DEFINE_LOG_CATEGORY(Logmmorpg)
+ 
